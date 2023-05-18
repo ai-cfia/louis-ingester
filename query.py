51167f775168c8af3fcad8e3d9e95ec05907428c
Ricky Ng-Adam <Ricky.Ng-Adam@inspection.gc.ca>
2023-05-18 19:06:00 +0000

    remove builtin packages

--- query.py
+++ query.py
@@ -4,10 +4,11 @@ model = index_module.CohereModel()
 model_index = index_module.get_index(model.index_name())
 print(model_index.describe_index_stats())
 
-query = "Find founding acts for organizations created by the government"
+query = "What is the CFIA founding mandate?"
 
 # create the query vector
 xq = model.encode(query)
+
 # print(xq)
 # now query
 xc = model_index.query(xq, top_k=5, include_metadata=True)
